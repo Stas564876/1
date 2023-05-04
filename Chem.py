e24e8f1734e141795e66deb50a11c70dce0bde2d
Stas564876 <91433348+Stas564876@users.noreply.github.com>
2023-05-04 16:54:12 +0300

    Add files via upload

--- Chem.py
+++ Chem.py
@@ -1,8 +1,19 @@
 import tkinter
+
+
+
 def main():
     
     tk = tkinter.Tk()
-
+    tk.geometry("400x300")
+    m_enab = int()
+    what_we_have_text = tkinter.Label(text = "Що ми маємо?   Відміть відповідні велечини")
+    m = tkinter.Checkbutton(text = "Маса",variable=m_enab)
+    
+    
+    
+    what_we_have_text.grid(column=0,row=0)
+    m.grid(column=0,row=1,sticky="w")
     tk.mainloop()
     
     
